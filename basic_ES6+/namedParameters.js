49e98a2cbe013bcd2d49753f9c6e270f18e42d56
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2022-02-07 21:48:01 +0900

    함수에 비구조화 할당 사용법

---
+++ basic_ES6+/namedParameters.js
@@ -0,0 +1,21 @@
+//Named Parameters
+
+function print_person1(name, age, region){
+    console.log(name, age, region);
+};
+
+print_person1('tom',10,'Seoul');
+
+function print_person2({name,age,retion}){
+    console.log(name, age, region);
+}
+
+print_person1({ name:'tom',age:10,region:'Seoul'});
+
+//위와 같이 객체를 만들어 비구조화 할당으로 사용하여 Named Parameters를 만든다
+
+//python 에서는 
+//def print_person(name, age, region):
+// print(name, age, region)
+//print_person('tom',10,'Seoul')
+//print_person(name='tom',age=10,region='Seoul') 둘다 사용가능 
