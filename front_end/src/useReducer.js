618a7f6f5c63827a7457c2e8e78a802b7d4ec316
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2022-02-12 19:58:34 +0900

    customhook

---
+++ front_end/src/useReducer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+const App= () => {
+    
+
+    return (
+        <div>
+            <input/>
+            <input/>
+        </div>
+    );
+}
+
+export default App;
